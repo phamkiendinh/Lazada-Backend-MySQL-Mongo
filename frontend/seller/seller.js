2ac6aef1c39fab5eaa272057e75f9afa187b0a97
locphan201 <u1275898@utah.edu>
2023-09-02 10:03:59 +0700

    Update Set Category

--- frontend/seller/seller.js
+++ frontend/seller/seller.js
@@ -7,13 +7,28 @@ const overlay = document.getElementById('overlay');
 const createFormContainer = document.getElementById('create-form-container');
 const updateFormContainer = document.getElementById('update-form-container');
 
+fetch ('http://127.0.0.1:3001/admin/category')
+    .then(response => response.json())
+    .then(categories => {
+        const createSelectedMenu = document.getElementById('create-category')
+        const updateSelectedMenu = document.getElementById('update-category')
+        categories.forEach(category => {
+            createSelectedMenu.innerHTML += `<option value="${category.name}">${category.name}</option>`;
+            updateSelectedMenu.innerHTML += `<option value="${category.name}">${category.name}</option>`;
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching products:', error);
+    });
+
+
 function fillInUpdateForm(productId) {
     fetch(`http://127.0.0.1:3001/products/${productId}`)
         .then(response => response.json())
         .then(product => {
             document.getElementById('update-id').textContent = product.id;
             document.getElementById('update-title-input').value = product.title;
-            // document.getElementById('update-title.input').value = product.category;
+            document.getElementById('update-category').value = product.category;
             document.getElementById('update-description-input').value = product.description;
             document.getElementById('update-price-input').value = product.price;
             document.getElementById('update-length-input').value = product.length;
@@ -84,78 +99,78 @@ fetch('http://127.0.0.1:3001/products')
 
 
 document.getElementById("create-image-input").addEventListener("change", function(event) {
-  var reader = new FileReader();
+    var reader = new FileReader();
 
-  reader.onload = function(e) {
-    const img = new Image();
-    img.src = e.target.result;
+    reader.onload = function(e) {
+        const img = new Image();
+        img.src = e.target.result;
 
-    img.onload = function() {
-      const width = img.width;
-      const height = img.height;
+        img.onload = function() {
+            const width = img.width;
+            const height = img.height;
 
-      let newWidth = width;
-      let newHeight = height;
+            let newWidth = width;
+            let newHeight = height;
 
-      if (width > maxWidth || height > maxHeight) {
-        const aspectRatio = width / height;
+            if (width > maxWidth || height > maxHeight) {
+                const aspectRatio = width / height;
 
-        if (width > maxWidth) {
-          newWidth = maxWidth;
-          newHeight = newWidth / aspectRatio;
-        }
+                if (width > maxWidth) {
+                    newWidth = maxWidth;
+                    newHeight = newWidth / aspectRatio;
+                }
 
-        if (newHeight > maxHeight) {
-          newHeight = maxHeight;
-          newWidth = newHeight * aspectRatio;
-        }
-      }
+                if (newHeight > maxHeight) {
+                    newHeight = maxHeight;
+                    newWidth = newHeight * aspectRatio;
+                }
+            }
 
-      createImageTag.src = img.src;
-      createImageTag.style.width = newWidth + "px";
-      createImageTag.style.height = newHeight + "px";
+            createImageTag.src = img.src;
+            createImageTag.style.width = newWidth + "px";
+            createImageTag.style.height = newHeight + "px";
+        };
     };
-  };
-  
-  reader.readAsDataURL(event.target.files[0]);
+    
+    reader.readAsDataURL(event.target.files[0]);
 });
 
 document.getElementById("update-image-input").addEventListener("change", function(event) {
     var reader = new FileReader();
   
     reader.onload = function(e) {
-      const img = new Image();
-      img.src = e.target.result;
-  
-      img.onload = function() {
-        const width = img.width;
-        const height = img.height;
-  
-        let newWidth = width;
-        let newHeight = height;
-  
-        if (width > maxWidth || height > maxHeight) {
-          const aspectRatio = width / height;
-  
-          if (width > maxWidth) {
-            newWidth = maxWidth;
-            newHeight = newWidth / aspectRatio;
-          }
-  
-          if (newHeight > maxHeight) {
-            newHeight = maxHeight;
-            newWidth = newHeight * aspectRatio;
-          }
-        }
-  
-        updateImageTag.src = img.src;
-        updateImageTag.style.width = newWidth + "px";
-        updateImageTag.style.height = newHeight + "px";
-      };
-    };
+        const img = new Image();
+        img.src = e.target.result;
     
+        img.onload = function() {
+            const width = img.width;
+            const height = img.height;
+    
+            let newWidth = width;
+            let newHeight = height;
+    
+            if (width > maxWidth || height > maxHeight) {
+                const aspectRatio = width / height;
+        
+                if (width > maxWidth) {
+                    newWidth = maxWidth;
+                    newHeight = newWidth / aspectRatio;
+                }
+        
+                if (newHeight > maxHeight) {
+                    newHeight = maxHeight;
+                    newWidth = newHeight * aspectRatio;
+                }
+            }
+    
+            updateImageTag.src = img.src;
+            updateImageTag.style.width = newWidth + "px";
+            updateImageTag.style.height = newHeight + "px";
+        };
+    };
+        
     reader.readAsDataURL(event.target.files[0]);
-  });
+});
 
 document.getElementById('create-button').addEventListener('click', () => {
     overlay.style.display = 'block';
@@ -176,17 +191,16 @@ document.getElementById('cancel-button-2').addEventListener('click', () => {
 });
 
 document.getElementById('form-create-button').addEventListener('click', async event => {
-    // formData.append('category', document.getElementById('create-category-input').value);
-
     const formData = new FormData()
     formData.append('title', document.getElementById('create-title-input').value);
     formData.append('description', document.getElementById('create-description-input').value);
+    formData.append('category', document.getElementById('create-category').value);
     formData.append('price', parseFloat(document.getElementById('create-price-input').value));
     formData.append('length', parseFloat(document.getElementById('create-length-input').value));
     formData.append('width', parseFloat(document.getElementById('create-width-input').value));
     formData.append('height', parseFloat(document.getElementById('create-height-input').value));
     
-    const imageInput = document.getElementById('update-image-input').files[0];
+    const imageInput = document.getElementById('create-image-input').files[0];
     formData.append('image', imageInput !== undefined ? imageInput : '');
 
     try {
@@ -232,12 +246,12 @@ document.getElementById('form-delete-button').addEventListener('click', async ()
 });
 
 document.getElementById('form-update-button').addEventListener('click', async () => {
-    // formData.append('category', document.getElementById('create-category-input').value);
     const productID = document.getElementById('update-id').textContent
 
     const formData = new FormData()
     formData.append('title', document.getElementById('update-title-input').value);
     formData.append('description', document.getElementById('update-description-input').value);
+    formData.append('category', document.getElementById('update-category').value);
     formData.append('price', parseFloat(document.getElementById('update-price-input').value));
     formData.append('length', parseFloat(document.getElementById('update-length-input').value));
     formData.append('width', parseFloat(document.getElementById('update-width-input').value));
